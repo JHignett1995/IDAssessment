4bed4d7583dc28f9de6c5b9e9a3821383bdd6514
JHignett1995 <jordan.hignett1@googlemail.com>
2019-04-26 13:00:11 +0100

    Fixed Format issues, changed regional to uppercase

--- main.tf
+++ main.tf
@@ -1,24 +1,28 @@
-provide = "google" {
- project = "IDMobile-Assignment"
- region = "europe-west2"
- zone = "europe-west2-a"
+provider "google" {
+  project = "{IDMobile-Assignment}"
+  region  = "europe-west2"
+  zone    = "europe-west2-a"
 }
-resource "google_compute_instance" "vm_instance"{
- name = "id-assignment-a"
- machine_type = "n1-standard-1"
- boot_disk {
-  initialize_params {
-   image = "centos-7-v20190423"
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "id-mobile-assignment"
+  machine_type = "n1-standard-1"
+
+  boot_disk {
+    initialize_params {
+      image = "centos-7-v20190423"
+    }
   }
- }
- network_interface {
-  network = "id-mobile-net"
-  access_config = {
+
+  network_interface {
+    network       = "{id-mobilt-net}"
+    access_config = {
+    }
   }
+}
+
+ resource "google_compute_network" "vpc_network"{
+  name = "id-mobile-net"
+  auto_create_subnetworks = "false"
+  routing_mode = "REGIONAL"
  }
-}
-resource "google_compute_network" "vpc_network"{
- name = "id-mobile-net"
- auto_create_subnetworks = "false"
- routing_mode = "regional"
-}
