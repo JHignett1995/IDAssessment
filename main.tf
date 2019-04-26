d4e145b5d17b1ca9176958e4e8140d904079eddf
JHignett1995 <jordan.hignett1@googlemail.com>
2019-04-26 12:09:15 +0100

    Added resources to complete instance and network

--- main.tf
+++ main.tf
@@ -3,6 +3,20 @@ provide = "google" {
  region = "europe-west2"
  zone = "europe-west2-a"
 }
+resource "google_compute_instance" "vm_instance"{
+ name = "id-assessment-a"
+ machine_type = "n1-standard-1"
+ boot_disk {
+  initialize_params {
+   image = "centos-7-v20190423"
+  }
+ }
+ network_interface {
+  network = "IDMobileAssignment-a"
+  access_config = {
+  }
+ }
+}
 resource "google_compute_network" "vpc_network"{
  name = "IDMobileAssignment-a"
  auto_create_subnetworks = "false"
